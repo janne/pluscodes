96b098138e901eab30d1d935ff64118240881fb1
Jan Andersson <jan.andersson@gmail.com>
2018-10-16 22:31:30 +0200

    Improve validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-const encode = coordinates => undefined
+const R = require('ramda')
+
+const digits = '23456789CFGHJMPQRVWX'
+
+const isValid = R.allPass([R.is(Object), R.has('longitude'), R.has('latitude')])
+
+const encode = (coordinates) => {
+  if (!isValid(coordinates)) return null
+  const { longitude, latitude } = coordinates
+  return ''
+}
+
 const decode = code => undefined
 
 module.exports = { encode, decode }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,17 @@
 const { encode, decode } = require('.')
 
 describe('encode', () => {
-  it('should return undefined if the input is undefined', () => {
-    expect(encode()).toEqual(undefined)
+  it('should return null if the input is undefined', () => {
+    expect(encode()).toEqual(null)
+  })
+
+  it('should return null if the input is not an object', () => {
+    expect(encode('foo')).toEqual(null)
+  })
+
+  it("should return null if the input doesn't contain longitude and latitude", () => {
+    expect(encode({})).toEqual(null)
+    expect(encode({ longitude: '' })).toEqual(null)
+    expect(encode({ latitude: '' })).toEqual(null)
   })
 })
