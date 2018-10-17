5ff909a170f894aa33c4efd297753e7abbbf2f44
Jan Andersson <jan.andersson@gmail.com>
2018-10-17 23:24:00 +0200

    Decode plus codes

--- src/decode.spec.js
+++ src/decode.spec.js
@@ -1,5 +1,7 @@
 const decode = require('./decode')
 
+const coords = { latitude: '59.327438', longitude: '18.054312' }
+
 it('returns null for non-string input', () => {
   expect(decode()).toEqual(null)
   expect(decode(42)).toEqual(null)
@@ -10,4 +12,8 @@ it('returns null for invalid length', () => {
   expect(decode('')).toEqual(null)
   expect(decode('23')).toEqual(null)
   expect(decode('23456789CFGH')).toEqual(null) // 12
+})
+
+it('decodes the plus code', () => {
+  expect(decode('9FFW83G3+XP')).toEqual({ latitude: '59.327375', longitude: '18.054250' })
 })
