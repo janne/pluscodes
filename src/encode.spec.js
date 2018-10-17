2e816b953b924a1211f20a7fd69aab18eff3b7de
Jan Andersson <jan.andersson@gmail.com>
2018-10-17 22:07:03 +0200

    Add support for lengths and handle invalid ones

--- src/encode.spec.js
+++ src/encode.spec.js
@@ -29,3 +29,33 @@ it('clamps latitudes below -90', () => {
   expect(encode({ latitude: '-90.0', longitude })).toEqual('2F2W2323+2P')
   expect(encode({ latitude: '-90.1', longitude })).toEqual('2F2W2323+2P')
 })
+
+it('has length 11 by default', () => {
+  expect(encode({ latitude, longitude }).length).toEqual(11)
+})
+
+it('it supports sending length and adds 1 (the plus)', () => {
+  expect(encode({ latitude, longitude }, 10).length).toEqual(11)
+  expect(encode({ latitude, longitude }, 8).length).toEqual(9)
+})
+
+it('adds zero padding on lengths below 8', () => {
+  expect(encode({ latitude, longitude }, 8)).toEqual('9FFW83G3+')
+  expect(encode({ latitude, longitude }, 6)).toEqual('9FFW8300+')
+  expect(encode({ latitude, longitude }, 4)).toEqual('9FFW0000+')
+  expect(encode({ latitude, longitude }, 2)).toEqual('9F000000+')
+})
+
+it('it only supports lengths above 2 and below 10', () => {
+  expect(encode({ latitude, longitude }, 0)).toEqual(null)
+  expect(encode({ latitude, longitude }, 1)).toEqual(null)
+  expect(encode({ latitude, longitude }, 11)).toEqual(null)
+  expect(encode({ latitude, longitude }, 12)).toEqual(null)
+})
+
+it('it only even lengths', () => {
+  expect(encode({ latitude, longitude }, 3)).toEqual(null)
+  expect(encode({ latitude, longitude }, 5)).toEqual(null)
+  expect(encode({ latitude, longitude }, 7)).toEqual(null)
+  expect(encode({ latitude, longitude }, 9)).toEqual(null)
+})
