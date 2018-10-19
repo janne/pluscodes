8638f8381f99bc81d2ddace22ce4b561c454cc86
Jan Andersson <jan.andersson@gmail.com>
2018-10-20 00:00:46 +0200

    Support short codes of length 5, 7 and 9

--- src/expand.js
+++ src/expand.js
@@ -3,16 +3,17 @@ const encode = require('./encode')
 const decode = require('./decode')
 const { digits } = require('./constants')
 
-const regexp = `^[${digits}]{4}[+][${digits}]{2}$`
+const regexp = `^[${digits}]{2}([${digits}]{2})?([${digits}]{2})?[+][${digits}]{2}$`
 const matchesDigits = str => Boolean(String(str).match(regexp))
 
 const isValid = R.allPass([matchesDigits, R.is(String)])
 
 const expand = (code, coordinates) => {
-  const prefixCode = encode(coordinates, 4)
   if (!isValid(code)) return null
+  const length = 11 - code.length
+  const prefixCode = encode(coordinates, length)
   if (!prefixCode) return null
-  return `${prefixCode.slice(0, 4)}${code}`
+  return `${prefixCode.slice(0, length)}${code}`
 }
 
 module.exports = expand
