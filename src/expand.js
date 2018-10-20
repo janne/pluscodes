e2cb54171b25b44735862c0c201e3fc9c6b56d98
Jan Andersson <jan.andersson@gmail.com>
2018-10-20 23:41:19 +0200

    Adjust expande coords if needed

--- src/expand.js
+++ src/expand.js
@@ -9,12 +9,27 @@ const matchesDigits = str => Boolean(String(str).match(regexp))
 
 const isValid = R.allPass([matchesDigits, R.is(String)])
 
-const expand = (code, coordinates) => {
-  if (!isValid(code)) return null
-  const length = 11 - code.length
-  const prefixCode = encode(coordinates, length)
-  if (!prefixCode) return null
-  return `${prefixCode.slice(0, length)}${code}`
+const adjust = (axis, refAxis, resolution) => {
+  if (axis > refAxis + resolution / 2) return axis - resolution
+  if (axis < refAxis - resolution / 2) return axis + resolution
+  return axis
+}
+
+const expand = (shortCode, ref) => {
+  if (!isValid(shortCode)) return null
+
+  const prefixLength = 11 - shortCode.length
+  const prefixedCode = encode(ref, prefixLength)
+  if (!prefixedCode) return null
+
+  const code = `${prefixedCode.slice(0, prefixLength)}${shortCode}`
+  const { latitude, longitude } = decode(code)
+  const resolution = Math.pow(20, 2 - prefixLength / 2)
+
+  return encode({
+    latitude: adjust(latitude, parseFloat(ref.latitude), resolution),
+    longitude: adjust(longitude, parseFloat(ref.longitude), resolution)
+  })
 }
 
 module.exports = expand
