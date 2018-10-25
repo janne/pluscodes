375294cf44a399a72dbb48a44b5946a16eee2d7f
Jan Andersson <jan.andersson@gmail.com>
2018-10-25 15:11:20 +0200

    Use floats by default

--- src/decode.spec.js
+++ src/decode.spec.js
@@ -20,31 +20,31 @@ it('returns null for invalid digits', () => {
 
 it('decodes the plus code', () => {
   expect(decode('9FFW84J9+XG')).toEqual({
-    latitude: '59.332438',
-    longitude: '18.118813',
+    latitude: 59.332438,
+    longitude: 18.118813,
     resolution: 0.000125
   })
 })
 
 it('decodes codes ending with +', () => {
   expect(decode('9FFW84J9+')).toEqual({
-    latitude: '59.331250',
-    longitude: '18.118750',
+    latitude: 59.33125,
+    longitude: 18.11875,
     resolution: 0.0025
   })
   expect(decode('9FFW8400+')).toEqual({
-    latitude: '59.325000',
-    longitude: '18.125000',
+    latitude: 59.325,
+    longitude: 18.125,
     resolution: 0.05
   })
   expect(decode('9FFW0000+')).toEqual({
-    latitude: '59.500000',
-    longitude: '18.500000',
+    latitude: 59.5,
+    longitude: 18.5,
     resolution: 1
   })
   expect(decode('9F000000+')).toEqual({
-    latitude: '60.000000',
-    longitude: '10.000000',
+    latitude: 60.0,
+    longitude: 10.0,
     resolution: 20
   })
 })
--- src/shorten.spec.js
+++ src/shorten.spec.js
@@ -12,7 +12,7 @@ it('returns full code on invalid reference', () => {
 })
 
 it('returns shortest code if reference is same as code', () => {
-  expect(shorten(code, { latitude: '59.332438', longitude: '18.118813' })).toEqual('J9+XG')
+  expect(shorten(code, { latitude: 59.332438, longitude: 18.118813 })).toEqual('J9+XG')
 })
 
 it('returns the shortened code on a local reference', () => {
@@ -20,9 +20,9 @@ it('returns the shortened code on a local reference', () => {
 })
 
 it('returns the shortened code on a national reference', () => {
-  expect(shorten(code, { latitude: '62.248996', longitude: '15.325620' })).toEqual('FW84J9+XG')
+  expect(shorten(code, { latitude: 62.248996, longitude: 15.32562 })).toEqual('FW84J9+XG')
 })
 
 it('returns the full code on a global reference', () => {
-  expect(shorten(code, { latitude: '59.329394', longitude: '-162.068712' })).toEqual(code)
+  expect(shorten(code, { latitude: 59.329394, longitude: -162.068712 })).toEqual(code)
 })
